24baca9ea239ed24a43bd6ba2bc95c8a2ec03752
Aman <amanjaiswal870@gmail.com>
2023-05-26 10:40:50 +0530

    Updated InputStreamReader.java

---
+++ InputStreamReader.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+
+class learningsssss {
+    public static void main(String[] args)
+    {
+    	System.out.println("Hello");
+        
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+        
+        InputStreamReader is = new InputStreamReader(System.in);
+        System.out.println(is);
+        
+    }
+}
